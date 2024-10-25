86e925b63d9d6c843d7e24154f7a9df5c3de9859
Haana <Haana@Haana>
2024-10-25 11:11:36 +0530

    UPDATE BASICS JAVA

---
+++ corejava/src/corejava/demo.java
@@ -0,0 +1,15 @@
+package corejava;
+import java.util.*;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner s= new  Scanner(System.in);
+       int a ,b,c;
+       a=5;
+       b=10;
+       c=a+b;
+       System.out.println(c);
+	}
+}
